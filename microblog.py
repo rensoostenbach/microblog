17a552ef6786c14e4a18eac83170eb5982722d2e
rensoostenbach <rensoostenbach@hotmail.com>
2018-01-14 18:27:12 +0100

    Revert "First commit"
    This reverts commit c3e642bda14f6f1a9fe80842e004f83ddb58768b.

--- microblog.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from app.models import User, Post
-from app.routes import *
-from app.forms import *
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
